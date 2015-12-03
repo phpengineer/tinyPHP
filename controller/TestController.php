20e26fc2a21f3c395ffa0dfe48f73fa145d29de8
xrh <540693750@qq.com>
2015-12-03 15:28:43 +0800

    完善测试类

--- controller/TestController.php
+++ controller/TestController.php
@@ -1,9 +1,5 @@
 <?php
 class TestController extends Controller {
-        
-        public function __construct() {
-                parent::__construct();
-        }
 
         public function index() {
         	$TestModel = $this->model('Test');
--- controller/homeController.php
+++ controller/homeController.php
@@ -1,9 +1,5 @@
 <?php
 class HomeController extends Controller {
-        
-        public function __construct() {
-                parent::__construct();
-        }
 
         public function index() {
         	$data['c'] = array('a' => 1, 'b' => 2);
